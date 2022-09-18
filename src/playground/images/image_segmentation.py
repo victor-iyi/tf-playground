c3d1c5c0792eb3df64a50a675db2592fe7d57f2f
Victor I. Afolabi <javafolabi@gmail.com>
2022-09-18 18:18:20 +0100

    Model subclassing for UNet model

--- src/playground/images/image_segmentation.py
+++ src/playground/images/image_segmentation.py
@@ -22,11 +22,13 @@ dataset, info = tfds.load(
     'oxford_iiit_pet:3.*.*', data_dir='../../data/', with_info=True
 )
 
-OUTPUT_CLASSES, VAL_SUBSPLITS = 3, 5
 IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL = 128, 128, 3
+IMG_SIZE = (IMG_HEIGHT, IMG_WIDTH)
+IMG_SHAPE = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL)
 
-EPOCHS, LEARNING_RATE = 20, 1e-4
+EPOCHS, LEARNING_RATE = 10, 1e-4
 BATCH_SIZE, BUFFER_SIZE = 64, 1000
+OUTPUT_CLASSES, VAL_SUBSPLITS = 3, 5
 
 TRAIN_LENGTH = info.splits['train'].num_examples
 VAL_STEPS = info.splits['test'].num_examples // BATCH_SIZE // VAL_SUBSPLITS
@@ -95,93 +97,177 @@ def load_image(
     return input_image, input_mask
 
 
-def display(display_lists: list[tf.TensorArray]) -> None:
+def display(images: list[tf.TensorArray]) -> None:
     plt.figure(figsize=(5, 5))
 
     title = ['Input Image', 'True Mask', 'Predicted Mask']
-    n_display = len(display_lists)
+    n_display = len(images)
 
-    for i, display_list in enumerate(display_lists):
+    for i, image in enumerate(images):
         plt.subplot(1, n_display, i + 1)
         plt.title(title[i])
-        plt.imshow(tf.keras.utils.array_to_img(display_list))
+        plt.imshow(tf.keras.utils.array_to_img(image))
         plt.axis('off')
 
     plt.show()
 
 
-def create_mask(pred_mask: tf.Tensor) -> tf.TensorArray:
-    pred_mask = tf.math.argmax(pred_mask, axis=-1)
-    pred_mask = pred_mask[..., tf.newaxis]
+def create_mask(pred_mask: tf.TensorArray) -> tf.TensorArray:
+    # pred_mask = tf.math.argmax(pred_mask, axis=-1)
+    # pred_mask = pred_mask[..., tf.newaxis]
     return pred_mask[0]
 
 
-def encoder_stack(
-    input_shape: tuple[int, int, int] = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL)
-) -> tf.keras.Model:
-    base_model = tf.keras.applications.MobileNetV2(
-        input_shape=input_shape, include_top=False
-    )
+class Encoder(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        input_shape: tuple[int, int, int] = IMG_SHAPE
+    ) -> None:
+        super(Encoder, self).__init__()
 
-    # Use the activations of these layers.
-    layer_names = [
-        'block_1_expand_relu',   # 64x64
-        'block_3_expand_relu',   # 32x32
-        'block_6_expand_relu',   # 16x16
-        'block_13_expand_relu',  # 8x8
-        'block_16_project',      # 4x4
-    ]
-    base_model_outputs = [base_model.get_layer(name).output
-                          for name in layer_names]
+        model = tf.keras.applications.MobileNetV2(
+            input_shape=input_shape, include_top=False
+        )
 
-    # Create the feature extraction model.
-    down_stack = tf.keras.Model(inputs=base_model.input,
-                                outputs=base_model_outputs)
-    down_stack.trainable = False
+        # Use the activations of these layers
+        layer_names = [
+            'block_1_expand_relu',   # 64x64
+            'block_3_expand_relu',   # 32x32
+            'block_6_expand_relu',   # 16x16
+            'block_13_expand_relu',  # 8x8
+            'block_16_project',      # 4x4
+        ]
 
-    return down_stack
+        model_output = [model.get_layer(name).output
+                        for name in layer_names]
+        self.encoder_model = tf.keras.Model(inputs=model.input,
+                                            outputs=model_output)
+        self.encoder_model.trainable = False
 
-
-def decoder_stack() -> list[tf.keras.Model]:
-    up_stack = [
-        pix2pix.upsample(512, 3),  # 4x4 -> 8x8
-        pix2pix.upsample(256, 3),  # 8x8 -> 16x16
-        pix2pix.upsample(128, 3),  # 16x16 -> 32x32
-        pix2pix.upsample(64, 3),   # 32x32 -> 64x64
-    ]
-    return up_stack
+    def call(
+        self, x: tf.TensorArray, training: bool = False,
+    ) -> tf.TensorArray:
+        return self.encoder_model(x, training=training)
 
 
-def unet_model(
-    output_channels: int = OUTPUT_CLASSES,
-    input_shape: tuple[int, int, int] = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL)
-) -> tf.keras.Model:
-    inputs = tf.keras.layers.Input(shape=input_shape)
+class UNet(tf.keras.Model):
+    def __init__(
+        self, input_shape: tuple[int, int, int] = IMG_SHAPE,
+        output_channels: int = OUTPUT_CLASSES,
+        dropout: float = 0.5,
+    ) -> None:
+        super(UNet, self).__init__()
 
-    # Downsampling through the model.
-    down_stack = encoder_stack(input_shape)
-    skips = down_stack(inputs)
-    x = skips[-1]
-    skips = reversed(skips[:-1])
+        # Don't train the encoder.
+        self.encoder.trainable = False
 
-    # Upsampling and establishing the skip connections.
-    up_stack = decoder_stack()
-    for up, skip in zip(up_stack, skips):
-        x = up(x)
-        concat = tf.keras.layers.Concatenate()
-        x = concat([x, skip])
+        decoder_filters = [512, 256, 128, 64]
+        initializer = tf.random_normal_initializer(0., 0.02)
+        self.concat = tf.keras.layers.Concatenate()
 
-    # This is the last layer of the model.
-    last = tf.keras.layers.Conv2DTranspose(
-        output_channels, 3, strides=2,
-        padding='same', activation='softmax'
-    )  # 64x64 -> 128x128
-    x = last(x)
+        # Decoder (upsampler).
+        self.decoder_stack = [tf.keras.Sequential([
+            tf.keras.layers.Conv2DTranspose(
+                filters, kernel_size=3, strides=2,
+                padding='same', use_bias=False,
+                kernel_initializer=initializer,
+            ),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dropout(dropout),
+            tf.keras.layers.ReLU(),
+        ]) for filters in decoder_filters]
 
-    return tf.keras.Model(inputs=inputs, outputs=x)
+        # Final (output) layer.
+        self.output_layer = tf.keras.layers.Conv2DTranspose(
+            filters=output_channels, kernel_size=3,
+            strides=2, padding='same',
+        )
+
+    def call(
+        self, inputs: tf.TensorArray, training: bool = False,
+    ) -> tf.TensorArray:
+        # Downsampling.
+        encoder_outputs = self.encoder(inputs, training=training)
+        x = encoder_outputs[-1]
+
+        # Skip connections.
+        skips = reversed(encoder_outputs[:-1])
+
+        # Upsampling and establishing the skip connections.
+        for decoder, skip in zip(self.decoder_stack, skips):
+            x = decoder(x)
+            x = self.concat([x, skip])
+
+        # Output mask.
+        output = self.output_layer
+        return output
 
 
-# TODO(victor): Double check `history` type.
+# def encoder_stack(
+#     input_shape: tuple[int, int, int] = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL)
+# ) -> tf.keras.Model:
+#     base_model = tf.keras.applications.MobileNetV2(
+#         input_shape=input_shape, include_top=False
+#     )
+
+#     # Use the activations of these layers.
+#     layer_names = [
+#         'block_1_expand_relu',   # 64x64
+#         'block_3_expand_relu',   # 32x32
+#         'block_6_expand_relu',   # 16x16
+#         'block_13_expand_relu',  # 8x8
+#         'block_16_project',      # 4x4
+#     ]
+#     base_model_outputs = [base_model.get_layer(name).output
+#                           for name in layer_names]
+
+#     # Create the feature extraction model.
+#     down_stack = tf.keras.Model(inputs=base_model.input,
+#                                 outputs=base_model_outputs)
+#     down_stack.trainable = False
+
+#     return down_stack
+
+
+# def decoder_stack() -> list[tf.keras.Model]:
+#     up_stack = [
+#         pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+#         pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+#         pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+#         pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+#     ]
+#     return up_stack
+
+
+# def unet_model(
+#     output_channels: int = OUTPUT_CLASSES,
+#     input_shape: tuple[int, int, int] = (IMG_HEIGHT, IMG_WIDTH, IMG_CHANNEL)
+# ) -> tf.keras.Model:
+#     inputs = tf.keras.layers.Input(shape=input_shape)
+
+#     # Downsampling through the model.
+#     down_stack = encoder_stack(input_shape)
+#     skips = down_stack(inputs)
+#     x = skips[-1]
+#     skips = reversed(skips[:-1])
+
+#     # Upsampling and establishing the skip connections.
+#     up_stack = decoder_stack()
+#     for up, skip in zip(up_stack, skips):
+#         x = up(x)
+#         concat = tf.keras.layers.Concatenate()
+#         x = concat([x, skip])
+
+#     # This is the last layer of the model.
+#     last = tf.keras.layers.Conv2DTranspose(
+#         output_channels, 3, strides=2,
+#         padding='same', activation='softmax'
+#     )  # 64x64 -> 128x128
+#     x = last(x)
+
+#     return tf.keras.Model(inputs=inputs, outputs=x)
+
+
 def plot_train_history(history: tf.keras.callbacks.History) -> None:
     loss = history.history['loss']
     val_loss = history.history['val_loss']
@@ -208,7 +294,8 @@ def main() -> int:
         sample_image, sample_mask = images[0], masks[0]
         display([sample_image, sample_mask])
 
-    model = unet_model(output_channels=OUTPUT_CLASSES)
+    # model = unet_model(output_channels=OUTPUT_CLASSES)
+    model = UNet(input_shape=IMG_SHAPE, output_channels=OUTPUT_CLASSES)
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -228,18 +315,19 @@ def main() -> int:
 
     plot_train_history(history)
 
-    # def show_predictions(
-    #     dataset: tf.dataset.Dataset | None = None,
-    #     num: int = 1
-    # ) -> None:
-    #     if dataset:
-    #         for image, mask in dataset.take(num):
-    #             pred_mask = model.predict(image)
-    #             display([image[0], mask[0], create_mask(pred_mask)])
-    #     else:
-    #         pred_mask = model.predict(sample_image[tf.newaxis, ...])
-    #         display([sample_image, sample_mask, create_mask(pred_mask)])
-    # show_predictions()
+    def show_predictions(
+        dataset: tf.dataset.Dataset | None = None,
+        num: int = 1
+    ) -> None:
+        if dataset:
+            for image, mask in dataset.take(num):
+                pred_mask = model.predict(image)
+                display([image[0], mask[0], create_mask(pred_mask)])
+        else:
+            pred_mask = model.predict(sample_image[tf.newaxis, ...])
+            display([sample_image, sample_mask, create_mask(pred_mask)])
+
+    show_predictions()
 
     return 0
 
